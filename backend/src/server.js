4d3466c8a9f6c6fcc5239cb2db48b30cacd42847
Virendra <narveer019@gmail.com>
2025-10-27 18:52:10 +0530

    deployment setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,39 @@
-import express from 'express'
-import notesRoutes from './routes/notesRoutes.js'
-import { dbConnect } from './config/db.js';
-import dotenv from 'dotenv'
-import rateLimiter from './middlewares/rateLimiter.js';
+import express from "express";
+import notesRoutes from "./routes/notesRoutes.js";
+import { dbConnect } from "./config/db.js";
+import dotenv from "dotenv";
+import path from "path";
+import rateLimiter from "./middlewares/rateLimiter.js";
 dotenv.config();
-import cors from 'cors';
-
-
+import cors from "cors";
 
 const app = express();
-app.use(cors());
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors());
+}
 app.use(express.json());
 app.use(rateLimiter);
 
+const __dirname = path.resolve();
+
 dbConnect()
-.then(()=>{
-    app.listen(process.env.PORT, ()=>{
-    console.log(`Server is running on port http://localhost:${process.env.PORT}`);
-})
-})
-.catch(()=>{
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `Server is running on port http://localhost:${process.env.PORT}`
+      );
+    });
+  })
+  .catch(() => {
     console.log("Failed to connect DB");
-})
+  });
 
-app.get('/',(req, res)=>{
-    res.send("something is here...")
-})
+app.use("/api/notes", notesRoutes);
 
-app.use("/api/notes", notesRoutes)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
